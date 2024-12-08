8b144971625f05e3edeb6aaab485014b78243e71
clari <jbvictoria1@up.edu.ph>
2024-12-09 00:02:35 +0800

    fix: change theme

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from ttkbootstrap.scrolled import ScrolledFrame
 from app import App
 
 if __name__ == "__main__":
-    root = ttk.Window(themename="darkly")
+    root = ttk.Window(themename="solar")
     root.title("CMSC 150 Project")
     root.geometry("1080x720")
     app = App(root)
